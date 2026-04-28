e7c1e68e9e628e7ab7074b6917fc2dd6301ffee3
Josue Samba Dianzungu <josamba-@fu-r2-p4.s19.be>
2026-04-28 17:13:08 +0200

    add: stack_size function

---
+++ stack_size.c
@@ -0,0 +1,19 @@
+#include "push_swap.h"
+
+int	ft_lstsize(t_stack *stack)
+{
+	int		length;
+	t_stack	*current;
+
+	current = stack;
+	length = 0;
+	if (current == NULL)
+		return (0);
+	while (current->next != NULL)
+	{
+		length++;
+		current = current->next;
+	}
+	length++;
+	return (length);
+}
